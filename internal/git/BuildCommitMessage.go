24163c93817e2df5184e8a96003ba4af0c4851fa
Jaques Boeno <jaquesboeno@proton.me>
2025-04-28 19:07:36 -0300

    feat: implements template string in build commit message
    closes #13

--- internal/git/BuildCommitMessage.go
+++ internal/git/BuildCommitMessage.go
@@ -15,6 +15,16 @@ func BuildCommitMessage(template string, answers map[string]string, questionList
 		}
 	}
 
+	current := questionList.Head
+
+	for current != nil {
+		if current.TemplateString != "" && answers[current.Key] != "" {
+			answers[current.Key] = strings.ReplaceAll(current.TemplateString, "<value>", answers[current.Key])
+		}
+
+		current = current.NextQuest
+	}
+
 	for key, value := range answers {
 		placeholder := fmt.Sprintf("<%s>", key)
 		message = strings.ReplaceAll(message, placeholder, value)
